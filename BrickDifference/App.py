e9f165ac8ce672373db761e9af3c4c4ca31559fb
Nexusnui <131207888+Nexusnui@users.noreply.github.com>
2025-03-28 11:59:37 +0100

    - Add existing project files

---
+++ BrickDifference/App.py
@@ -0,0 +1,368 @@
+import os
+import platform
+from enum import Enum
+from BrickDifference.Model_Functions import *
+
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QIcon
+from PyQt6.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QVBoxLayout,
+    QFormLayout,
+    QWidget,
+    QHBoxLayout,
+    QLineEdit,
+    QPushButton,
+    QGroupBox,
+    QCheckBox,
+    QComboBox,
+    QSpinBox,
+    QFileDialog,
+    QMessageBox,
+    QLabel
+)
+from PyQt6.QtCore import pyqtSignal
+
+app_version = "0.1"
+
+if platform.system() == "Windows":
+    try:
+        from ctypes import windll  # Only exists on Windows.
+
+        myappid = f"nexusnui.brickdifference.{app_version}"
+        windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
+    except ImportError:
+        pass
+
+basedir = os.path.dirname(__file__)
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle(f"Brick Difference {app_version}")
+        main_layout = QHBoxLayout()
+        sub_layout = QVBoxLayout()
+
+    # Input Files Area
+        input_files_area = QGroupBox("Input Files")
+        input_files_layout = QFormLayout()
+        input_files_area.setLayout(input_files_layout)
+
+        self.input_file_a = FileWidget("path/to/first/input/file", line_editable=False)
+        self.input_file_a.path_changed.connect(self.path_a_changed)
+        input_files_layout.addRow("File A", self.input_file_a)
+        self.input_file_b = FileWidget("path/to/second/input/file", line_editable=False)
+        input_files_layout.addRow("File B", self.input_file_b)
+
+    # Output Files Area
+        output_files_area = QGroupBox("Output Files")
+        output_files_layout = QFormLayout()
+        output_files_area.setLayout(output_files_layout)
+
+        self.output_file_a_and_b = FileWidget("path/to/output/file/a/and/b",
+                                              True, existing_file=False,
+                                              default_filename="BD_in_A_and_B.ldr")
+        a_and_b_label = QLabel("In A and B ℹ️")
+        a_and_b_label.setToolTip("Partlist: Parts appearing in both A and B\n"
+                                 "Difference Model: Geometry A and B have in common")
+        output_files_layout.addRow(a_and_b_label, self.output_file_a_and_b)
+        self.output_file_only_a = FileWidget("path/to/output/file/only/a",
+                                             True, existing_file=False,
+                                             default_filename="BD_only_in_A.ldr")
+        only_a_label = QLabel("Only in A ℹ️")
+        only_a_label.setToolTip("Partlist: Parts only appearing A\n"
+                                 "Difference Model: Geometry only A has")
+        output_files_layout.addRow(only_a_label, self.output_file_only_a)
+        self.output_file_only_b = FileWidget("path/to/output/file/only/b",
+                                             True, existing_file=False,
+                                             default_filename="BD_only_in_B.ldr")
+        only_b_label = QLabel("Only in B ℹ️")
+        only_b_label.setToolTip("Partlist: Parts only appearing B\n"
+                                "Difference Model: Geometry only B has")
+        output_files_layout.addRow(only_b_label, self.output_file_only_b)
+
+    # Settings Area
+        control_area = QGroupBox("Settings")
+        control_layout = QFormLayout()
+        control_area.setLayout(control_layout)
+
+        self.mode_input = QComboBox()
+        self.mode_input.addItems(["Partlist", "Difference Model"])
+        self.mode_input.setEditable(False)
+        self.mode_input.currentIndexChanged.connect(self.mode_changed)
+        self.mode = Mode.PARTLIST
+        control_layout.addRow("Mode", self.mode_input)
+
+        self.column_distance_input = QSpinBox()
+        self.column_distance_input.setMinimum(1)
+        self.column_distance_input.setMaximum(1000)
+        self.column_distance_input.setValue(165)
+        column_label = QLabel("Column Distance ℹ️")
+        column_label.setToolTip("Distance between part types in partlist model.\n"
+                                "Distance in LDraw Units (20ldu = 1 stud)")
+        control_layout.addRow(column_label, self.column_distance_input)
+
+        self.row_distance_input = QSpinBox()
+        self.row_distance_input.setMinimum(1)
+        self.row_distance_input.setMaximum(1000)
+        self.row_distance_input.setValue(165)
+        row_label = QLabel("Row Distance ℹ️")
+        row_label.setToolTip("Distance between part colours in partlist model.\n"
+                             "Distance in LDraw Units (20ldu = 1 stud)")
+        control_layout.addRow(row_label, self.row_distance_input)
+
+        self.height_distance_input = QSpinBox()
+        self.height_distance_input.setMinimum(1)
+        self.height_distance_input.setMaximum(1000)
+        self.height_distance_input.setValue(35)
+        row_label = QLabel("Height Distance ℹ️")
+        row_label.setToolTip("Height between parts of the same type and colour in partlist model.\n"
+                             "Distance in LDraw Units (24ldu = 1 brick)")
+        control_layout.addRow("Height Distance", self.height_distance_input)
+
+        self.generate_button = QPushButton("Generate Difference Files")
+        self.generate_button.clicked.connect(self.generate_files)
+        control_layout.addRow(self.generate_button)
+
+    # Add Elements to Main Layout
+
+        sub_layout.addWidget(input_files_area)
+        sub_layout.addWidget(output_files_area)
+        main_layout.addLayout(sub_layout)
+        main_layout.addWidget(control_area)
+        widget = QWidget()
+        widget.setLayout(main_layout)
+        self.setCentralWidget(widget)
+
+    def mode_changed(self, index):
+        partlist_disabled = index != Mode.PARTLIST.value
+        self.mode = Mode(index)
+        self.column_distance_input.setDisabled(partlist_disabled)
+        self.row_distance_input.setDisabled(partlist_disabled)
+        self.height_distance_input.setDisabled(partlist_disabled)
+
+    def path_a_changed(self, path):
+        parent_dir = os.path.dirname(path)
+        self.output_file_a_and_b.set_default_filename(parent_dir, "BD_in_A_and_B.ldr")
+        self.output_file_only_a.set_default_filename(parent_dir, "BD_only_in_A.ldr")
+        self.output_file_only_b.set_default_filename(parent_dir, "BD_only_in_B.ldr")
+
+    def generate_files(self):
+        filepath_a = self.input_file_a.get_current_path()
+        filepath_b = self.input_file_b.get_current_path()
+        filepath_a_and_b = self.output_file_a_and_b.get_current_path()
+        generate_a_and_b = self.output_file_a_and_b.is_activated
+        filepath_only_a = self.output_file_only_a.get_current_path()
+        generate_only_a = self.output_file_only_a.is_activated
+        filepath_only_b = self.output_file_only_b.get_current_path()
+        generate_only_b = self.output_file_only_b.is_activated
+        if self.mode == Mode.PARTLIST:
+            column_distance = self.column_distance_input.value()
+            row_distance = self.row_distance_input.value()
+            height_distance = self.height_distance_input.value()
+
+        if not (generate_only_a or generate_only_b or generate_a_and_b):
+            QMessageBox.critical(self, "Output Disabled", "All Outputfiles are disabled")
+            return
+
+        error_messages = []
+        has_critical = False
+        has_acceptable = False
+
+        a_exists, _, a_msg = self.input_file_a.validate_filepath()
+        if not a_exists:
+            has_critical = True
+            error_messages.append(a_msg)
+
+        b_exists, _, b_msg = self.input_file_b.validate_filepath()
+        if not b_exists:
+            has_critical = True
+            error_messages.append(b_msg)
+
+        a_and_b_exists, a_and_b_is_dir, a_and_b_msg = self.output_file_a_and_b.validate_filepath()
+        if not a_and_b_is_dir and generate_a_and_b:
+            has_critical = True
+            error_messages.append(a_and_b_msg)
+        elif a_and_b_exists and generate_a_and_b:
+            has_acceptable = True
+            error_messages.append(a_and_b_msg)
+
+        a_only_exists, a_only_is_dir, a_only_msg = self.output_file_only_a.validate_filepath()
+        if not a_only_is_dir and generate_only_a:
+            has_critical = True
+            error_messages.append(a_only_msg)
+        elif a_only_exists and generate_only_a:
+            has_acceptable = True
+            error_messages.append(a_only_msg)
+
+        b_only_exists, b_only_is_dir, b_only_msg = self.output_file_only_b.validate_filepath()
+        if not b_only_is_dir and generate_only_b:
+            has_critical = True
+            error_messages.append(b_only_msg)
+        elif b_only_exists and generate_only_b:
+            has_acceptable = True
+            error_messages.append(b_only_msg)
+
+        if has_critical:
+            print("".join(error_messages))
+            QMessageBox.critical(self, "Filepath Error(s)", f"At least one input file does not exist.\n"
+                                                            f"All Errors/Warnings:\n"
+                                                            f"{"".join(error_messages)}")
+            return
+        elif has_acceptable:
+            answer = QMessageBox.warning(
+                self,
+                "Output File(s) already exists",
+                f"One or more Outputfiles alreay exist:\n{"".join(error_messages)}\nOverride?",
+                QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No
+            )
+            if answer == QMessageBox.StandardButton.No:
+                return
+
+        filetree_a = LdrawFileTree(filepath_a)
+        filetree_b = LdrawFileTree(filepath_b)
+
+        if generate_only_b:
+            if self.mode == Mode.PARTLIST:
+                out_only_b, out_a_and_b = get_part_difference(
+                    list(filetree_b.filetree.values())[0].get_total_partlist(),
+                    list(filetree_a.filetree.values())[0].get_total_partlist()
+                )
+            elif self.mode == Mode.DIFF_MODEL:
+                out_only_b, out_a_and_b = get_difference_model(filetree_b, filetree_a)
+
+        if generate_only_a:
+            if self.mode == Mode.PARTLIST:
+                out_only_a, out_a_and_b = get_part_difference(
+                    list(filetree_a.filetree.values())[0].get_total_partlist(),
+                    list(filetree_b.filetree.values())[0].get_total_partlist()
+                )
+            elif self.mode == Mode.DIFF_MODEL:
+                out_only_a, out_a_and_b = get_difference_model(filetree_a, filetree_b)
+
+        if not generate_only_a and not generate_only_b and generate_a_and_b:
+            if self.mode == Mode.PARTLIST:
+                _, out_a_and_b = get_part_difference(
+                    list(filetree_a.filetree.values())[0].get_total_partlist(),
+                    list(filetree_b.filetree.values())[0].get_total_partlist()
+                )
+            elif self.mode == Mode.DIFF_MODEL:
+                _, out_a_and_b = get_difference_model(filetree_a, filetree_b)
+
+        saved_files = []
+        if self.mode == Mode.PARTLIST:
+            if generate_a_and_b:
+                out_a_and_b.save_as_ldraw_file(filepath_a_and_b, column_distance, row_distance, height_distance)
+                saved_files.append(f"{filepath_a_and_b}\n")
+            if generate_only_a:
+                out_only_a.save_as_ldraw_file(filepath_only_a, column_distance, row_distance, height_distance)
+                saved_files.append(f"{filepath_only_a}\n")
+            if generate_only_b:
+                out_only_b.save_as_ldraw_file(filepath_only_b, column_distance, row_distance, height_distance)
+                saved_files.append(f"{filepath_only_b}\n")
+        elif self.mode == Mode.DIFF_MODEL:
+            if generate_a_and_b:
+                save_model(out_a_and_b, filepath_a_and_b)
+                saved_files.append(f"{filepath_a_and_b}\n")
+            if generate_only_a:
+                save_model(out_only_a, filepath_only_a)
+                saved_files.append(f"{filepath_only_a}\n")
+            if generate_only_b:
+                save_model(out_only_b, filepath_only_b)
+                saved_files.append(f"{filepath_only_b}\n")
+        QMessageBox.information(self, "Files Saved", f"Saved the following files:\n{"".join(saved_files)}")
+
+
+class FileWidget(QWidget):
+    path_changed = pyqtSignal(str)
+
+    def __init__(self, placeholder: str = "",
+                 has_checkbox: bool = False, line_editable: bool = True,
+                 existing_file: bool = True, default_filename=""):
+        super().__init__()
+        main_layout = QHBoxLayout()
+        self.line_editable = line_editable
+        self.existing_file = existing_file
+        self.default_filename = default_filename
+        self.filepath_input = QLineEdit()
+        if not self.line_editable:
+            self.filepath_input.setDisabled(True)
+        self.filepath_input.setPlaceholderText(placeholder)
+        main_layout.addWidget(self.filepath_input)
+        self.select_button = QPushButton("Select")
+        main_layout.addWidget(self.select_button)
+        self.select_button.clicked.connect(self.select_file)
+        self.has_checkbox = has_checkbox
+        self.is_activated = True
+        if self.has_checkbox:
+            self.checkbox = QCheckBox()
+            self.checkbox.setCheckState(Qt.CheckState.Checked)
+            main_layout.addWidget(self.checkbox)
+            self.checkbox.checkStateChanged.connect(self.checkstate_changed)
+        self.setLayout(main_layout)
+
+    def select_file(self):
+        filepath = ""
+        if self.existing_file:
+            dialog = QFileDialog(self)
+            dialog.setFileMode(QFileDialog.FileMode.ExistingFile)
+            dialog.setNameFilter("LDraw Model (*.ldr)")
+            dialog.setViewMode(QFileDialog.ViewMode.Detail)
+            if dialog.exec():
+                filepath = dialog.selectedFiles()[0]
+        else:
+            filepath, _ = QFileDialog.getSaveFileName(
+                self, "part save location", self.default_filename, "LDraw Model (*.ldr)"
+            )
+        if filepath and len(filepath) > 0:
+            self.filepath_input.setText(filepath)
+            self.path_changed.emit(filepath)
+
+    def checkstate_changed(self, check: Qt.CheckState):
+        set_disabled = check != Qt.CheckState.Checked
+        self.is_activated = check == Qt.CheckState.Checked
+        if self.line_editable:
+            self.filepath_input.setDisabled(set_disabled)
+        self.select_button.setDisabled(set_disabled)
+
+    def validate_filepath(self):
+        filepath = self.get_current_path()
+        file_exists = os.path.isfile(filepath)
+        dir_exists = os.path.isdir(os.path.dirname(filepath))
+        message = "Nothing Wrong"
+        if self.existing_file:
+            if not file_exists:
+                message = f"'{filepath}, does not exist\n"
+        else:
+            if file_exists:
+                message = f"'{filepath}' already exists\n"
+            if not dir_exists:
+                message = f"The Directory: '{os.path.dirname(filepath)} does not exist\n"
+        return file_exists, dir_exists, message
+
+    def get_current_path(self) -> str:
+        return self.filepath_input.text()
+
+    def set_default_filename(self, def_dir, name):
+        self.default_filename = os.path.join(def_dir, name)
+
+
+class Mode(Enum):
+    PARTLIST = 0
+    DIFF_MODEL = 1
+def run():
+    app = QApplication([0])
+    app.setWindowIcon(QIcon(os.path.join(basedir, "icons", "BrickDifference_Icon.ico")))
+
+    window = MainWindow()
+
+    window.show()
+
+    app.exec()
+
+
+if __name__ == "__main__":
+    run()
---
+++ BrickDifference/Model_Functions.py
@@ -0,0 +1,292 @@
+import time
+import os
+
+
+class Partlist:
+    def __init__(self, partlist: dict = None):
+        if partlist is not None:
+            self.partlist = partlist
+        else:
+            self.partlist = {}
+
+    def add_part(self, p_id: str, amount: int = 1):
+        if p_id in self.partlist:
+            self.partlist[p_id] += amount
+        else:
+            self.partlist[p_id] = amount
+
+    def add_part_by_line(self, line: str):
+        parameters = line.strip("\n").split(" ")
+        colour = parameters[1]
+        name = " ".join(parameters[14:])
+        self.add_part(f"{colour}:{name}")
+
+    def get_total_part_count(self) -> int:
+        count = 0
+        for part in self.partlist:
+            count += self.partlist[part]
+        return count
+
+    def generate_ldraw_model(self, filename, col_distance=165, row_distance=165, height_distance=35):
+        header = [
+            f"0 FILE {filename}\n",
+            "0 Untitled Model\n",
+            f"0 Name:  {filename}\n",
+            "0 Author: \n",
+            "0 CustomBrick\n",
+            "0 FlexibleBrickControlPointUnitLength -1\n",
+            f"0 NumOfBricks:  {self.get_total_part_count()}\n"
+        ]
+        content = []
+        rows = {}
+        columns = {}
+        part_ids = list(self.partlist.keys())
+
+        def part_id_to_partnumber(part_id: str):
+            name = part_id.split(":")[1].split(".dat")[0]
+            digits = []
+            post_chars = False
+            for char in name:
+                if not char.isdigit() and post_chars:
+                    break
+                if char.isdigit():
+                    post_chars = True
+                    digits.append(char)
+            if len(digits) > 0:
+                return int("".join(digits))
+            else:
+                return -1
+
+        col_count = 0
+        part_ids.sort(key=part_id_to_partnumber)
+        for part_id in part_ids:
+            partname = part_id.split(":")[1]
+            if partname not in columns:
+                columns[partname] = col_count
+                col_count += 1
+
+        def colour_id_to_number(part_colour: str):
+            colour_id = part_colour.split(":")[0]
+            if colour_id.isdigit():
+                return int(colour_id)
+            else:
+                return -1
+
+        row_count = 0
+        part_ids.sort(key=colour_id_to_number)
+        for part_id in part_ids:
+            colour = part_id.split(":")[0]
+            if colour not in rows:
+                rows[colour] = row_count
+                row_count += 1
+
+        for part in self.partlist:
+            colour, partname = part.split(":")
+            column = columns[partname]
+            row = rows[colour]
+            for i in range(self.partlist[part]):
+                new_line = f"1 {colour} {column * col_distance} {-i * height_distance} {row * row_distance} 1 0 0 0 1 0 0 0 1 {partname}\n"
+                content.append(new_line)
+
+        return header + content + ["0 NOFILE\n"]
+
+    def save_as_ldraw_file(self, filepath, col_distance=165, row_distance=165, height_distance=35):
+        filename = os.path.basename(filepath)
+        lines = self.generate_ldraw_model(filename, col_distance, row_distance, height_distance)
+        with open(filepath, "w", encoding="utf-8") as file:
+            file.writelines(lines)
+
+    def __str__(self):
+        return str(self.partlist)
+
+    def __repr__(self):
+        return f"Parlist -> Unique:{len(self.partlist)}, Total: {self.get_total_part_count()}"
+
+    def __add__(self, other):
+        if not isinstance(other, Partlist):
+            raise ValueError
+        sum_list = Partlist(other.partlist.copy())
+        for part in self.partlist:
+            sum_list.add_part(part, self.partlist[part])
+        return sum_list
+
+    def __mul__(self, other):
+        prod_list = Partlist(self.partlist.copy())
+        if not isinstance(other, int):
+            raise ValueError
+        for part in prod_list.partlist:
+            prod_list.partlist[part] *= other
+        return prod_list
+
+
+class LDrawFile:
+    def __init__(self, content: list):
+        self.header = []
+        self.filename = content[0].strip("\n")[7:]
+        self.modelname = content[1].strip("\n")[2:]
+        self.parts = Partlist()
+        self.submodels = {}
+        self.content = []
+        header_complete = False
+        for line in content:
+            if line.startswith("1"):
+                header_complete = True
+            if line.startswith("0 NumOfBricks"):
+                pass
+            elif not header_complete:
+                self.header.append(line)
+            elif line.startswith("1"):
+                self.content.append(line)
+                if line.endswith(".dat\n"):
+                    self.parts.add_part_by_line(line)
+                else:
+                    submodel_id = " ".join(line.strip("\n").split(" ")[14:])
+                    if submodel_id in self.submodels:
+                        self.submodels[submodel_id][1] += 1
+                    else:
+                        self.submodels[submodel_id] = [None, 1]
+
+    def link_submodel(self, submodel):
+        sub_id = submodel.filename.lower()
+        self.submodels[sub_id][0] = submodel
+
+    def get_total_partlist(self) -> Partlist:
+        total_list = Partlist(self.parts.partlist.copy())
+        for sub_id in self.submodels:
+            total_list += self.submodels[sub_id][0].get_total_partlist() * self.submodels[sub_id][1]
+        return total_list
+
+    def get_ldraw_lines(self):
+        return self.header + self.content + ["0 NOFILE\n"]
+
+    def __eq__(self, other):
+        if not isinstance(other, LDrawFile):
+            raise ValueError
+        self.content.sort()
+        other.content.sort()
+        if self.filename != other.filename:
+            return False
+        if len(self.content) != len(other.content):
+            return False
+        for index, line in enumerate(self.content):
+            if line != other.content[index]:
+                return False
+        return True
+
+
+class LdrawFileTree:
+    def __init__(self, filepath: str):
+        self.filetree = {}
+        with open(filepath, "r", encoding="utf-8") as file:
+            submodel_content = []
+            for line in file:
+                submodel_content.append(line)
+                if line.startswith("0 NOFILE"):
+                    if len(submodel_content) > 0:
+                        submodel = LDrawFile(submodel_content)
+                        self.filetree[submodel.filename.lower()] = submodel
+                        submodel_content = []
+        for fileid in self.filetree:
+            file: LDrawFile = self.filetree[fileid]
+            for submodel in file.submodels:
+                file.link_submodel(self.filetree[submodel])
+
+
+def get_difference_model(model_a: LdrawFileTree, model_b: LdrawFileTree):
+    difference_model = {}
+    common_model = {}
+    for file_id in model_a.filetree:
+        diff_file = LDrawFile(model_a.filetree[file_id].header)
+        comm_file = LDrawFile(model_a.filetree[file_id].header)
+        if file_id in model_b.filetree:
+            file_a = model_a.filetree[file_id]
+            file_b = model_b.filetree[file_id]
+            for line in file_a.content:
+                if line in file_b.content:
+                    if not line.endswith(".dat\n"):
+                        sub_id = " ".join(line.strip("\n").split(" ")[14:])
+                        if model_a.filetree[sub_id] != model_b.filetree[sub_id]:
+                            diff_file.content.append(line)
+                    comm_file.content.append(line)
+                else:
+                    if line.endswith(".dat\n"):
+                        diff_file.content.append(line)
+                    else:
+                        sub_id = " ".join(line.strip("\n").split(" ")[14:])
+                        if sub_id not in model_b.filetree or model_a.filetree[sub_id] == model_b.filetree[sub_id]:
+                            diff_file.content.append(line)
+                        else:
+                            new_sub_header = model_a.filetree[sub_id].header.copy()
+                            time_id = hex(time.time_ns())[2:]
+                            new_sub_id = f"{model_a.filetree[sub_id].filename.lower()}_{time_id}"
+                            #Case ID already used in model
+                            while new_sub_id in model_a.filetree:
+                                time_id = hex(time.time_ns())[2:]
+                                new_sub_id = f"{model_a.filetree[sub_id].filename.lower()}_{time_id}"
+
+                            new_sub_header[0] = new_sub_header[0].replace("\n", f"_{time_id}\n")
+                            new_sub_header[1] = new_sub_header[1].replace("\n", f"_{time_id}\n")
+                            new_sub_header[2] = new_sub_header[2].replace("\n", f"_{time_id}\n")
+                            new_sub = LDrawFile(new_sub_header+model_a.filetree[sub_id].content)
+                            difference_model[new_sub_id] = new_sub
+                            new_line = line.replace("\n", f"_{time_id}\n")
+                            diff_file.content.append(new_line)
+            common_model[file_id] = comm_file
+            if len(diff_file.content) > 0:
+                difference_model[file_id] = diff_file
+            else:
+                diff_file.content = file_a.content
+                difference_model[file_id] = diff_file
+        else:
+            diff_file.content = model_a.filetree[file_id].content
+            difference_model[file_id] = diff_file
+    return difference_model, common_model
+
+
+def save_model(model: dict, filepath):
+    with open(filepath, "w", encoding="utf-8") as file:
+        for ld_file in model.values():
+            file.writelines(ld_file.get_ldraw_lines())
+
+
+def get_part_difference(partlist_a: Partlist, partlist_b: Partlist):
+    diff_list = Partlist()
+    comm_list = Partlist()
+
+    for part in partlist_a.partlist:
+        if part in partlist_b.partlist:
+            if partlist_a.partlist[part] <= partlist_b.partlist[part]:
+                comm_list.partlist[part] = partlist_a.partlist[part]
+            elif partlist_a.partlist[part] > partlist_b.partlist[part]:
+                comm_list.partlist[part] = partlist_b.partlist[part]
+                diff = partlist_a.partlist[part] - partlist_b.partlist[part]
+                diff_list.partlist[part] = diff
+        else:
+            diff_list.partlist[part] = partlist_a.partlist[part]
+    return diff_list, comm_list
+
+if __name__ == "__main__":
+    testfile_a = "C:\\Users\\Hageta\\PycharmProjects\\BrickDifference_Pre\\TestmodelA.ldr"
+    testfile_b = "C:\\Users\\Hageta\\PycharmProjects\\BrickDifference_Pre\\TestmodelB.ldr"
+    #"""
+    file_tree_a = LdrawFileTree(testfile_a)
+    file_tree_b = LdrawFileTree(testfile_b)
+    part_diff, part_comm = get_part_difference(list(file_tree_a.filetree.values())[0].get_total_partlist(), list(file_tree_b.filetree.values())[0].get_total_partlist())
+
+    #rint(part_diff)
+    #print(part_comm)
+    for line in part_comm.generate_ldraw_model("diff_partlist", 85, 25, 25):
+        print(line.strip("\n"))
+    #"""
+    """
+    file_diff, file_comm = get_difference_model(file_tree_b, file_tree_a)
+
+    for sub in file_diff.values():
+        sub.content.sort()
+        for lline in sub.get_ldraw_lines():
+            print(lline.strip("\n"))
+    #"""
+
+
+    #total = list(file_tree_a.filetree.values())[0].get_total_partlist()
+    #print(total)
